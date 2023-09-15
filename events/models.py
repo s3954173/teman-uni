b6bfb2dd000f0b8dea114c3e65713c6b652d8ec8
s3940407 <s3940407@student.rmit.edu.au>
2023-09-15 21:06:37 +1000

    Update models.py

--- events/models.py
+++ events/models.py
@@ -1,25 +1,45 @@
-# from django.db import models
-# from django.core.exceptions import ValidationError
-# from django.utils.translation import gettext_lazy as _
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 # # Create your models here.
 
-# def validate_word_limit(value):
-#     word_limit = 100
-#     words = value.split()
-#     if len(words) > word_limit:
-#         raise ValidationError(
-#             _('Event description cannot exceed %(limit)s words.'),
-#             params={'limit':word_limit},
-#         )
+def validate_word_limit(value):
+    word_limit = 100
+    words = value.split()
+    if len(words) > word_limit:
+        raise ValidationError(
+            _('Event description cannot exceed %(limit)s words.'),
+            params={'limit':word_limit},
+        )
 
-# class Event(models.Model):
-#     eventName = models.CharField(max_length=100)
-#     eventDate = models.DateField()
-#     eventTime = models.TimeFields()
-#     eventDesc = models.TextField(max_length=1000)
-#     eventID = models.AutoField(primary_key=True)
-#     eventAdmin = models.ForeignKey(on_delete=models.CASCADE)
+class Event(models.Model):
+    eventName = models.CharField(max_length=100)
+    eventDate = models.DateField()
+    eventTime = models.TimeFields()
+    eventDesc = models.TextField(max_length=1000)
+    eventID = models.AutoField(primary_key=True)
+    eventAdmin = models.ForeignKey(on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return self.eventName
+    def __str__(self):
+        return self.eventName
+    
+    
+class SubmitEvent(models.Model):
+    eventName = models.CharField(max_length=100)
+    eventDate = models.DateField()
+    eventTime = models.TimeFields()
+    eventDesc = models.TextField(max_length=1000)
+    eventID = models.AutoField(primary_key=True)
+    eventAdmin = models.ForeignKey(on_delete=models.CASCADE)
+    creatorID = models.OneToOneField(on_delete=models.CASCADE, related_name='creator_profile')
+
+    def __str__(self):
+        return self.eventName
+
+class InvitedFriends(models.Model):
+    friendID = models.ForeignKey(on_delete=models.CASCADE)
+    eventID = models.ForeignKey('Event', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Invited friend: {self.friendID.username} for Event: {self.eventID.eventName}"
