23ef7c7fa114085e14a0c9b855f9e83c5c77e709
klarisaarwena <s3951662@student.rmit.edu.au>
2023-09-17 21:44:56 +1000

    messaging django attempt

---
+++ messaging/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from users.models import User
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sender_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='receiver_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
